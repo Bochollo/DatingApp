b6f088188005f3188b95593e12d62c97048586fc
Gerhard Rogg <gerhard.rogg@web.de>
2021-03-14 23:33:15 +0100

    Just before Bootstrap

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { AstMemoryEfficientTransformer } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  users: any;
+  title = 'The New Dating App';
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    console.log('PROTO: OnInit');
+    this.getUsers();
+  }
+
+  getUsers() {
+    console.log('getUsers: START');
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+    console.log('SUCCESS');
+    this.users = response;
+
+    }, error => {
+      console.log('ERRROR');
+      console.log(AstMemoryEfficientTransformer);
+    })
+  
+
+  }
+
+
+
+}
